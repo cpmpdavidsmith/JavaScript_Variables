e7cf2dd9fc5971d2918eabf50c5fbee220fc0f4c
David Smith <cpmpdavidsmith@icloud.com>
2023-03-09 11:06:14 -0500

    java scrip Variables

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Solid";
+let lastName = "Snake";
+var age = 51;
 // Exercise 2
 
+lastName = "Slate";
+age = 32;
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first release in ${createdYear}.');
+//Exercise 4 
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0; 
+let inStock = false;
+let selectedSize = "M";
+
+console.log('${product} in size ${selectedSize} is $${price} with ${stock} in stock.'
+);
+//Exercise 5 
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+letbookmark = 456;
+let hasread = true;
+
+console.log(
+  '${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.'
+);
